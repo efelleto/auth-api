0f21f7ee97c507fc3586495c0a92fc93f298ad3d
efelleto <eduardofelleto@icloud.com>
2024-07-26 05:52:53 -0300

    Initial commit

---
+++ src/test/java/dev/felleto/auth_api/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felleto.auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
