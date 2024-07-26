43825538655d9815fc1be609beae5a3743ecaec3
efelleto <eduardofelleto@icloud.com>
2024-07-26 06:24:31 -0300

    Entity and initial security methods commit

---
+++ src/main/java/dev/felleto/auth_api/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.felleto.auth_api.repositories;
+
+import dev.felleto.auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
